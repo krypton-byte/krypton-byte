6dd5de3986db1b27a8c8d163ca8338a6e0536c37
krypton-byte <krypton-byte@localhost.localdomain>
2021-06-24 23:19:46 +0700

    web page

---
+++ porfolio/js.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    mode = {
+        night:{
+            background:"black",
+            icon:"white"
+        },
+        light:{
+            background:"white",
+            icon:"black"
+        }
+    }
+    mode_ = "night"
+    function href(url){
+        window.open(url, '_blank');
+    }
+    $("#moon").on("click", switchMode)
+    function switchMode(){
+        color = mode_==='light'?mode.night:mode.light
+        mode_ = mode_==='light'?'night':'light'
+        $("p")[0].style.color = color.icon;
+        $("body").css("background",color.background);
+        for(var x of $("box-icon")){
+            x.attributes.color.value = color.icon;
+        }
+        for(var x of $("li").find("a")){
+            x.style.color = color.icon;
+        }
+    }
+
+})
